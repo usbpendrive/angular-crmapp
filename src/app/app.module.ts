08a7e231a1fc4d3ab71211a9cd46410504e42073
Fernando <ferry.fernando@gmail.com>
2018-10-31 16:21:37 +0700

    Components generation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { LeadListComponent } from './lead-list/lead-list.component';
+import { LeadCreateComponent } from './lead-create/lead-create.component';
+import { OpportunityListComponent } from './opportunity-list/opportunity-list.component';
+import { OpportunityCreateComponent } from './opportunity-create/opportunity-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountListComponent,
+    AccountCreateComponent,
+    ContactListComponent,
+    ContactCreateComponent,
+    LeadListComponent,
+    LeadCreateComponent,
+    OpportunityListComponent,
+    OpportunityCreateComponent
   ],
   imports: [
     BrowserModule
